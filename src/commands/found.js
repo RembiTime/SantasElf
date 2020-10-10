cae2749c183bb00a173499acdd568e2fa9cb47de
Samsamson33 <samsamson33.0913@gmail.com>
2020-10-10 18:42:16 -0400

    Format, remove unused variables

--- src/commands/found.js
+++ src/commands/found.js
@@ -10,40 +10,36 @@ class FoundCommand extends Command {
 
 	async exec(message) {
 		const args = message.content.slice(6).trim().split(" ");
-		/*const command = args.shift().toLowerCase();
-    if (!args.length) {
-        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-    }
-    else {*/
-		let query = this.client.database.query(`SELECT * FROM presents WHERE code = "${args[0]}"`, (err, result) => {
+
+		this.client.database.query(`SELECT * FROM presents WHERE code = "${args[0]}"`, (err, result) => {
 			if (err) throw err;
 			if (result.length == 0) {
 				message.channel.send("That present does not exist!");
 			} else {
-				let query = this.client.database.query(`SELECT presentLevel, timesFound FROM presents WHERE code = '${args[0]}'`, (err, result) => {
+				this.client.database.query(`SELECT presentLevel, timesFound FROM presents WHERE code = '${args[0]}'`, (err, result) => {
 					if (err) throw err;
 					console.log(result);
 				});
 
-        if (result[0].timesFound == 0) {
-          let timesFound = result[0].timesFound
-          timesFound = timesFound + 1
-          let query = this.client.database.query(`UPDATE presents SET timesFound = ${timesFound} WHERE code = '${args[0]}'`, (err, result) => {
-	           if(err) throw err;
-	           console.log(result);
-            })
-          message.channel.send("You were the first one to find this present! It had a difficulty of `" + result[0].presentLevel + "`.")
-        } else {
-          let timesFound = result[0].timesFound
-          timesFound = timesFound + 1
-          let query = this.client.database.query(`UPDATE presents SET timesFound = ${timesFound} WHERE code = '${args[0]}'`, (err, result) => {
-             if(err) throw err;
-             console.log(result);
-            })
-				message.channel.send("You just claimed the present! It had a difficulty of `" + result[0].presentLevel + "`.");
-      }
+				if (result[0].timesFound == 0) {
+					let timesFound = result[0].timesFound;
+					timesFound = timesFound + 1;
+					this.client.database.query(`UPDATE presents SET timesFound = ${timesFound} WHERE code = '${args[0]}'`, (err, result) => {
+						if (err) throw err;
+						console.log(result);
+					});
+					message.channel.send("You were the first one to find this present! It had a difficulty of `" + result[0].presentLevel + "`.");
+				} else {
+					let timesFound = result[0].timesFound;
+					timesFound = timesFound + 1;
+					this.client.database.query(`UPDATE presents SET timesFound = ${timesFound} WHERE code = '${args[0]}'`, (err, result) => {
+						if (err) throw err;
+						console.log(result);
+					});
+					message.channel.send("You just claimed the present! It had a difficulty of `" + result[0].presentLevel + "`.");
+				}
+			}
 		}
-  }
 		);
 	}
 }
--- src/commands/hide.js
+++ src/commands/hide.js
@@ -11,21 +11,23 @@ class HideCommand extends Command {
 	async exec(message) {
 		const args = message.content.slice(5).trim().split(" ");
 		if (args.length == 1) {
-		    return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 		} else {
 			let serverID = message.guild.id;
-      let serverName = message.guild.name;
+			let serverName = message.guild.name;
 			let channelID = message.channel.id;
-      let channelName = message.channel.name;
+			let channelName = message.channel.name;
 			let userID = message.author.id;
-      let userName = message.member.user.tag;
-			let tspecs = {code: `${args[0]}`, presentLevel: args[1], timesFound: 0, serverName: `${serverName}`, serverID: serverID, channelName: `${channelName}`, channelID: channelID, hiddenByName: `${userName}`, hiddenByID: userID};
-			let query = this.client.database.query("INSERT INTO presents SET ?", tspecs, (err, result) => {
+			let userName = message.member.user.tag;
+			let tspecs = { code: `${args[0]}`, presentLevel: args[1], timesFound: 0, serverName: `${serverName}`, serverID: serverID, channelName: `${channelName}`, channelID: channelID, hiddenByName: `${userName}`, hiddenByID: userID };
+			this.client.database.query("INSERT INTO presents SET ?", tspecs, (err, result) => {
 				message.channel.send("Created a present with the code of `" + args[0] + "` and a difficulty of `" + args[1] + "`.");
-	     if(err) throw err;
-	     console.log(result);
+				if (err) throw err;
+				console.log(result);
 			}
-			);}}
+			);
+		}
+	}
 }
 
 module.exports = HideCommand;
