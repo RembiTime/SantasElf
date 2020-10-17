a65ba21e5818b92ef36a406fc5623803608ab390
Samsamson33 <samsamson33.0913@gmail.com>
2020-10-16 22:10:12 -0400

    Format table creation SQL

--- src/Database.js
+++ src/Database.js
@@ -7,10 +7,64 @@ class Database {
 
 	async init() {
 		await Promise.all([
-			this.pool.execute("CREATE TABLE IF NOT EXISTS presents (id int AUTO_INCREMENT, code VARCHAR(255), presentLevel int, timesFound int, guildName VARCHAR(255), guildID BIGINT UNSIGNED, channelName VARCHAR(255), channelID BIGINT UNSIGNED, hiddenByName VARCHAR(255), hiddenByID BIGINT UNSIGNED, PRIMARY KEY(id))"),
-			this.pool.execute("CREATE TABLE IF NOT EXISTS userData (userID BIGINT UNSIGNED, userName VARCHAR(255), wrongGuesses int, firstFinder int, totalPresents int, lvl1Presents int, lvl1Total int, lvl2Presents int, lvl2Total int, lvl3Presents int, lvl3Total int, item1 int, item2 int, item3 int, PRIMARY KEY(userID))"),
-			this.pool.execute("CREATE TABLE IF NOT EXISTS foundPresents (id int AUTO_INCREMENT, userID BIGINT UNSIGNED, userName VARCHAR(255), presentCode VARCHAR(255), PRIMARY KEY(id))"),
-			this.pool.execute("CREATE TABLE IF NOT EXISTS staffApproval (messageID BIGINT UNSIGNED, status ENUM('ONGOING', 'ACCEPTED', 'DENIED'), claimedByID BIGINT UNSIGNED, code VARCHAR(255), presentLevel int, guildName VARCHAR(255), guildID BIGINT UNSIGNED, channelName VARCHAR(255), channelID BIGINT UNSIGNED, hiddenByName VARCHAR(255), hiddenByID BIGINT UNSIGNED)"),
+			this.pool.execute(`
+				CREATE TABLE IF NOT EXISTS presents (
+					id            INTEGER AUTO_INCREMENT,
+					code          VARCHAR(255),
+					presentLevel  INTEGER,
+					timesFound    INTEGER,
+					guildName     VARCHAR(255),
+					guildID       BIGINT UNSIGNED,
+					channelName   VARCHAR(255),
+					channelID     BIGINT UNSIGNED,
+					hiddenByName  VARCHAR(255),
+					hiddenByID    BIGINT UNSIGNED,
+					PRIMARY KEY(id)
+				)
+			`),
+			this.pool.execute(`
+				CREATE TABLE IF NOT EXISTS userData (
+					userID         BIGINT UNSIGNED,
+					userName       VARCHAR(255),
+					wrongGuesses   INTEGER,
+					firstFinder    INTEGER,
+					totalPresents  INTEGER,
+					lvl1Presents   INTEGER,
+					lvl1Total      INTEGER,
+					lvl2Presents   INTEGER,
+					lvl2Total      INTEGER,
+					lvl3Presents   INTEGER,
+					lvl3Total      INTEGER,
+					item1          INTEGER,
+					item2          INTEGER,
+					item3          INTEGER,
+					PRIMARY KEY(userID)
+				)
+			`),
+			this.pool.execute(`
+				CREATE TABLE IF NOT EXISTS foundPresents (
+					id           INTEGER AUTO_INCREMENT,
+					userID       BIGINT UNSIGNED,
+					userName     VARCHAR(255),
+					presentCode  VARCHAR(255),
+					PRIMARY KEY(id)
+				)
+			`),
+			this.pool.execute(`
+				CREATE TABLE IF NOT EXISTS staffApproval (
+					messageID     BIGINT UNSIGNED,
+					status        ENUM('ONGOING', 'ACCEPTED', 'DENIED'),
+					claimedByID   BIGINT UNSIGNED,
+					code          VARCHAR(255),
+					presentLevel  INTEGER,
+					guildName     VARCHAR(255),
+					guildID       BIGINT UNSIGNED,
+					channelName   VARCHAR(255),
+					channelID     BIGINT UNSIGNED,
+					hiddenByName  VARCHAR(255),
+					hiddenByID    BIGINT UNSIGNED
+				)
+			`)
 		]);
 	}
 
