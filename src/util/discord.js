3451d3240ed70d84d61b35e05186f8c19babbe27
Samsamson33 <samsamson33.0913@gmail.com>
2020-10-21 18:45:36 -0400

    Add showPages util

---
+++ src/util/discord.js
@@ -0,0 +1,49 @@
+const showPages = async function(pages, channel, user, time = 30000) {
+	const message = await channel.send(pages[0]);
+	const collector = message.createReactionCollector(() => true);
+
+	const noop = () => {};
+
+	const left = "◀️";
+	const right = "▶️";
+	const end = "⏹️";
+
+	let page = 0;
+	let timeout;
+
+	collector.on("collect", (reaction, reactor) => {
+		if (reactor.id === reactor.client.user.id) { return; }
+		reaction.users.remove(reactor).catch(noop);
+
+		if (reactor.id !== user.id) { return; }
+
+		if (reaction.emoji.name === left) {
+			page--;
+			if (page < 0) { page += pages.length; }
+
+			message.edit(pages[page]);
+			clearTimeout(timeout);
+			timeout = setTimeout(() => collector.stop(), time);
+		} else if (reaction.emoji.name === right) {
+			page++;
+			page %= pages.length;
+
+			message.edit(pages[page]);
+			clearTimeout(timeout);
+			timeout = setTimeout(() => collector.stop(), time);
+		} else if (reaction.emoji.name === end) {
+			collector.stop();
+		}
+	});
+
+	collector.once("end", async () => {
+		clearTimeout(timeout);
+		message.delete();
+	});
+
+	await message.react(left);
+	await message.react(end);
+	await message.react(right);
+};
+
+module.exports = { showPages };
