fa00f60f80a30bf45b632057e9529c331985e760
RPS1222 <47305744+RPS1222@users.noreply.github.com>
2020-11-10 23:02:20 -0600

    Fixed ,found to work with temphide

--- src/commands/found.js
+++ src/commands/found.js
@@ -21,11 +21,13 @@ class FoundCommand extends Command {
 		let finderIsHider = false;
 		let alreadyFound = false;
 		let firstFinder = false;
+		let tempPresent = false;
+		let presentExpired = false;
 
 		await this.client.knex.transaction(async trx => {
 			[present] = await this.client.knex("presents")
-				.select("timesFound", "hiddenByID", "presentLevel")
-				.where({ code, guildID: message.guild.id })
+				.select("timesFound", "hiddenByID", "presentLevel", "usesLeft")
+				.where({ code })
 				.transacting(trx)
 				.forUpdate();
 
@@ -51,7 +53,11 @@ class FoundCommand extends Command {
 				return;
 			} else {
 				if (present.usesLeft === 0) {
-					message.channel.send("This code has expired!");
+					presentExpired = true;
+					return;
+				} else if (present.usesLeft !== null) {
+					await this.client.knex("presents").decrement("usesLeft", 1).where({ code: code }).transacting(trx);
+					tempPresent = true;
 				}
 
 				await this.client.knex("foundPresents")
@@ -73,21 +79,25 @@ class FoundCommand extends Command {
 					firstFinder = true;
 				}
 			}
+
 		});
 
-		if (present === null) {
-			// TODO: increment wrong guesses counter?
+		if (!present) {
+		// TODO: increment wrong guesses counter?
 			await message.channel.send("That present does not exist!");
 		} else if (finderIsHider) {
 			await message.channel.send("You can't claim a present that you hid!");
 		} else if (alreadyFound) {
 			await message.channel.send("You've already claimed that present!");
-		} else if (firstFinder) {
+		} else if (presentExpired) {
+			await message.channel.send("This code has expired!");
+		} else if (firstFinder && !tempPresent) {
 			await message.channel.send(`You were the first one to find this present! It had a difficulty of \`${present.presentLevel}\`.`);
 		} else {
 			await message.channel.send(`You just claimed the present! It had a difficulty of \`${present.presentLevel}\`.`);
 		}
 	}
 }
+
 
 module.exports = FoundCommand;
