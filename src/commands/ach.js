0e03965bc26efcec0d1d6b856f4747a0c7a61947
RPS1222 <rps1222yt@gmail.com>
2020-11-11 14:14:43 -0600

    small ,ach progress

--- src/commands/ach.js
+++ src/commands/ach.js
@@ -2,6 +2,7 @@ const { Command } = require("discord-akairo");
 const { partition } = require("../util/array");
 const { showPages } = require("../util/discord");
 const { MessageEmbed } = require("discord.js");
+const achievements = require("./achievements");
 
 class AchievementCommand extends Command {
 	constructor() {
@@ -13,19 +14,19 @@ class AchievementCommand extends Command {
 
 	async exec(message) {
 		const userData = await this.client.database.userDataCheck({ userID: message.author.id });
-
 		const hexColor = Math.random() < 0.5 ? "#FF5A5A" : "#8DFF5A";
+
+		let completeSquares = (await Promise.all([1, 2, 3, 4].map(x => this.client.database.checkAchievement({ name: `serverPresents${x}`, userID: message.author.id })))).map(Number);
+		let achProgress = completeSquares.findIndex(0);
+		let achName = achievements.find(ach => ach.keyID === "serverPresents");
+		completeSquares.map(x=>x ? "🟩" : "🟥");
 
 		const page1 = new MessageEmbed()
 			.setColor(hexColor)
 			.setTitle("Achievements")
 			.setFooter("Page 1")
-			let checkach = await this.client.database.checkAchievement({ name: "presentTotal", userID: message.author.id });
-			if (checkach) {
-				page1.addField("Christmas Spirit", "🟩🟥🟥🟥🟥🟥")
-			} else {
-				page1.addField("Christmas Spirit", "🟥🟥🟥🟥🟥🟥")
-			}
+			.addField(completeSquares);
+
 
 		const page2 = new MessageEmbed()
 			.setColor(hexColor)
--- src/commands/ping.js
+++ src/commands/ping.js
@@ -10,6 +10,7 @@ class PingCommand extends Command {
 
 	async exec(message) {
 		await message.channel.send("Pong!");
+		this.client.database.foundAchievement({achName: "presentTotal", userID: message.author.id, message: message});
 	}
 }
 
