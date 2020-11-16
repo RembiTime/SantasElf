7ab32a1a9c4883b3ede5e0b6787f8249a7289b4c
Samsamson33 <samsamson33.0913@gmail.com>
2020-11-16 00:53:18 -0500

    Update addPartner command

--- src/commands/addPartner.ts
+++ src/commands/addPartner.ts
@@ -1,35 +1,27 @@
 import { Command } from "discord-akairo";
-import { Util } from "discord.js";
+import { Util, Message, Guild } from "discord.js";
 
 class AddPartnerCommand extends Command {
 	constructor() {
 		super("addPartner", {
 			aliases: ["addPartner"],
 			description: "Adds a partner",
+			channel: "guild",
 			ownerOnly: true
 		});
 	}
 
-	async exec(message) {
-		// This could double-send the success message if run twice in quick succession,
-		// but the race condition is harmless
+	async exec(message: Message & { guild: Guild }) {
+		await this.client.knex.transaction(async trx => {
+			const isPartner = await message.guild.isPartner(trx);
 
-		await message.guild.ensureDB();
-
-		const [{ isPartner }] = await this.client.knex("guildData")
-			.select("isPartner")
-			.where({ guildID: message.guild.id });
-
-		if (isPartner) {
-			message.channel.send("This guild is already partnered!");
-			return;
-		} else {
-			await this.client.knex("guildData")
-				.update({ isPartner: true })
-				.where({ guildID: message.guild.id });
-
-			await message.channel.send(`**${Util.escapeMarkdown(message.guild.name)}** is now a partner!`);
-		}
+			if (isPartner) {
+				await message.channel.send("This guild is already partnered!");
+			} else {
+				await message.guild.setPartner(true, trx);
+				await message.channel.send(`**${Util.escapeMarkdown(message.guild.name)}** is now a partner!`);
+			}
+		});
 	}
 }
 
