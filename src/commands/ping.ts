3052641fbcad46cbb26c611bc369cfb7a33c4195
RPS1222 <rps1222yt@gmail.com>
2020-11-25 15:19:46 -0600

    small

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -11,6 +11,7 @@ class PingCommand extends Command {
 
 	async exec(message: Message) {
 		await message.channel.send("Pong!");
+		this.client.database.addLog(message.author.id + " pinged!")
 	}
 }
 
