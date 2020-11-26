3d74514ecf7849d52d91af6ff93dfc2a049f1266
TNThacker2015 <tnthacker2015@gmail.com>
2020-11-25 20:26:45 -0800

    inline

---
+++ src/commands/help.ts
@@ -0,0 +1,25 @@
+import { Command } from "discord-akairo";
+import { Message, MessageEmbed } from "discord.js";
+import { showPages } from "../util/discord";
+import { chunk } from "../util/array";
+
+class HelpCommand extends Command {
+	constructor() {
+		super("help", {
+			aliases: ["help", "?", "rpspleasehelpihavenoideawhattodo"],
+			description: "Shows a list of commands",
+		});
+	}
+
+	async exec(message: Message) {
+		const embedArr: MessageEmbed[] = [];
+		for (const commands of chunk(this.client.commandHandler.modules.array(), 10)) {
+			const embed = new MessageEmbed();
+			for (const command of commands) embed.addField(command.id, command.description, true);
+			embedArr.push(embed);
+		}
+		showPages(embedArr, message.channel, message.author, 120000);
+	}
+}
+
+export = HelpCommand;
