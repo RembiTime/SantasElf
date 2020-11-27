836244e359a892605e9bcee0de411b76fc74335e
TNThacker2015 <tnthacker2015@gmail.com>
2020-11-27 10:42:54 -0800

    start

--- start.js
+++ start.js
@@ -1,14 +1,11 @@
-const { execSync, fork } = require("child_process");
+const { exec, fork } = require("child_process");
 console.log("Building TypeScript...");
-try {
-	console.log(execSync("npx tsc") + "");
-} catch (e) {
-	console.log(e.toString());
-	console.log(e.stderr + []);
-}
+exec("npx tsc", (err, out, std) => console.log(err ? std : out));
+
+/*
 console.log("Starting bot...");
 const child = fork("./dist/index.js");
 child.on("message", m => console.log(m.toString()));
 child.on("error", err => console.error(err.toString()));
 child.on("exit", m => (console.log(`Exited with ${m}`), process.exit(1)));
-child.on("disconnect", () => (console.log("Process exited."), process.exit(0)));
+child.on("disconnect", () => (console.log("Process exited."), process.exit(0)));*/
