d48684111110720da4ac03e060e933379e436c3e
RPS1222 <rps1222yt@gmail.com>
2020-11-27 13:03:48 -0600

    Added public log for adding a partner

--- src/commands/addPartner.ts
+++ src/commands/addPartner.ts
@@ -1,5 +1,6 @@
 import { Command } from "discord-akairo";
-import { Util, Message, Guild } from "discord.js";
+import { Util, Message, Guild, TextChannel } from "discord.js";
+import channels from "../channels.json";
 
 class AddPartnerCommand extends Command {
 	constructor() {
@@ -19,6 +20,9 @@ class AddPartnerCommand extends Command {
 				await message.channel.send("This guild is already partnered!");
 			} else {
 				await message.guild.setPartner(true, trx);
+				const publicLogs = await this.client.channels.cache.get(channels.publicLogs) as TextChannel;
+				let { guild } = message;
+				publicLogs.send("**" + guild.name + "** is not a partner!");
 				await message.channel.send(`**${Util.escapeMarkdown(message.guild.name)}** is now a partner!`);
 				this.client.database.addLog(`${message.guild.name} has been added as a partner`);
 			}
