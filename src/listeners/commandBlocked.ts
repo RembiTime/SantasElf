fb34490d0c93b91f2783710cbf4af644d6e33803
Samsamson33 <samsamson33.0913@gmail.com>
2020-11-27 00:31:20 -0500

    Typescriptify listeners

--- src/listeners/commandBlocked.ts
+++ src/listeners/commandBlocked.ts
@@ -1,4 +1,4 @@
-const { Listener } = require("discord-akairo");
+import { Listener } from "discord-akairo";
 
 class MissingPermsListener extends Listener {
 	constructor() {
--- src/listeners/missingPermissions.ts
+++ src/listeners/missingPermissions.ts
@@ -1,4 +1,4 @@
-const { Listener } = require("discord-akairo");
+import { Listener } from "discord-akairo";
 
 class BlockedCmdListener extends Listener {
 	constructor() {
